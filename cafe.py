310c25fd6a7e0b2414e8587bed6dcd09bb4f7636
gurungpritam870-art <gurungpritam870@gmail.com>
2026-02-21 15:53:23 +0545

    add cafe.py

---
+++ cafe.py
@@ -0,0 +1,54 @@
+print("hello")
+
+cafe="mauro cafe"
+print(cafe)
+
+_city="Pokhara"
+print(_city)
+
+n=200
+n=+400
+N=-200
+print(n)
+complex_num=2j+3j
+print(complex_num)
+
+X=1200
+Y=500
+
+print(X+Y)
+print(X-Y)
+print(X*Y)
+print(X/Y) # floating point divison
+print(X/Y) # floor divison
+
+
+a=20
+if a > 21:
+    print("loss")
+elif a > 18:
+    print('profit')
+
+
+names=["aa","bb","cc","cc"]  
+print(names[0])
+print(names[1])
+print(names[2])
+print(names[3])
+names.append("DD")
+print(names)
+names.remove("cc")
+print(names)
+names.pop(0)
+
+print(names)
+
+cast=("apple","banana","cherry")
+print(cast)
+swarna={"a","d","s"}
+swarna.remove("a")
+print(swarna)
+
+for i in range(9):
+    print(i)
+    
