23ea35a28751e60cfa64e63c28e77953cdb41f06
gurungpritam870-art <gurungpritam870@gmail.com>
2026-02-21 14:45:37 +0545

    add hello.py

--- hello.py
+++ hello.py
@@ -1,6 +1,14 @@
 noun=["go","run","eat","go","go"]
 
-noun_2=["drink","rest","and"]
 
-print(noun+noun_2)
+print(noun[0])
+print(noun[1])
+print(noun[2])
+print(noun[3])
+print(noun[4])
+noun.pop(0)
+noun.pop(3)
+print(noun)
 
+#thelist={"aa","bb","cc","cc","cc"}
+#Print(thelist)
